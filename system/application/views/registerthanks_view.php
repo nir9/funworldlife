a59a45b4070ebbfb760fd12d83bf762a311e7a18
Nir Lichtman@NIR-EEPC <unknown>
2008-12-13 21:27:32 +0200

    Putting Thanks After Registering

--- system/application/views/registerthanks_view.php
+++ system/application/views/registerthanks_view.php
@@ -1,10 +1,11 @@
-
+﻿
 <?php 
 	$this->load->view("header.php", $title) 
 ?>
 
-		<div dir='rtl'>
-		
+		<div dir='rtl' align="center">
+		<h1><font color="red">תודה רבה שנרשמת לאתר שלנו מקווים שתהנו</font></h1>
+		<input type="submit" onclick="window.location='javascript:history.back(-1)'" value="לחץ כאן כדי להתחיל לשחק">
 		</div>
 	</body>
 </html>
