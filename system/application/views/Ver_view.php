0911b9a75ae51fb004c80cdb0d511310c9728a02
Nir Lichtman <nir_lichtman@hotmail.com>
2009-03-27 16:02:30 +0300

    Added VER Pages

--- system/application/views/Ver_view.php
+++ system/application/views/Ver_view.php
@@ -8,14 +8,126 @@
 	<br>
 	<br>
 	<div align="center" dir="ltr" class="footer" style="background: cyan; width: 970;">
-	V3.01 BETA
+	V3.01 BETA 9/6/09
 	</div>
 	<br>
-	באגים מתוקנים:
 	<br>
 	תיקון באגים - בעבודת הזבל היה אפשר להרוויח כפול כסף
 	<br>
-	תיקון באגים - 
+	שינוי - ברחוב 4 התבטל המדבר
+	<br>
+	הוספה - הוסף מפת העולם ודף הראשי
+	<br>
+	הוספה - נוספו עוד 15 רחובות סך הכול 20 רחובות
+	<br>
+	הוספה - בנית בתים ברחובות
+	<br>
+	הוספה - השכרת דירות
+	<br>
+	<br>
+	<div align="center" dir="ltr" class="footer" style="background: cyan; width: 970;">
+	V3.00 BETA 1/1/09
+	</div>
+	<br>
+	שינוי - אכשיו יש אתר חדש
+	<br>
+	<br>
+	<div align="center" dir="ltr" class="footer" style="background: cyan; width: 970;">
+	V2.90 BETA 3/9/08
+	</div>
+	<br>
+	הוספה - חנויות אישיות
+	<br>
+	<br>
+	<div align="center" dir="ltr" class="footer" style="background: cyan; width: 970;">
+	V2.80 BETA 3/6/08
+	</div>
+	<br>
+	
+	<br>
+	<br>
+	<div align="center" dir="ltr" class="footer" style="background: cyan; width: 970;">
+	V2.70 BETA 3/4/08
+	</div>
+	<br>
+	
+	<br>
+	<br>
+	<div align="center" dir="ltr" class="footer" style="background: cyan; width: 970;">
+	V2.60 BETA 3/3/08
+	</div>
+	<br>
+	
+	<br>
+	<br>
+	<div align="center" dir="ltr" class="footer" style="background: cyan; width: 970;">
+	V2.50 BETA 3/1/08
+	</div>
+	<br>
+	
+	<br>
+	<br>
+	<div align="center" dir="ltr" class="footer" style="background: cyan; width: 970;">
+	V2.40 BETA 3/12/07
+	</div>
+	<br>
+	
+	<br>
+	<br>
+	<div align="center" dir="ltr" class="footer" style="background: cyan; width: 970;">
+	V2.30 BETA 1/11/07
+	</div>
+	<br>
+	
+	<br>
+	<br>
+	<div align="center" dir="ltr" class="footer" style="background: cyan; width: 970;">
+	V2.20 BETA 3/10/07
+	</div>
+	<br>
+	
+	<br>
+	<br>
+	<div align="center" dir="ltr" class="footer" style="background: cyan; width: 970;">
+	V2.10 BETA 3/9/07
+	</div>
+	<br>
+	
+	<br>
+	<br>
+	<div align="center" dir="ltr" class="footer" style="background: cyan; width: 970;">
+	V2.00 BETA 3/6/07
+	</div>
+	<br>
+	שינוי - אכשיו יש אתר חדש
+	<br>
+	<br>
+	<div align="center" dir="ltr" class="footer" style="background: cyan; width: 970;">
+	V1.50 BETA 3/5/07
+	</div>
+	<br>
+	
+	<br>
+	<br>
+	<div align="center" dir="ltr" class="footer" style="background: cyan; width: 970;">
+	V1.00 BETA 1/1/07
+	</div>
+	<br>
+	
+	<br>
+	<br>
+	<div align="center" dir="ltr" class="footer" style="background: cyan; width: 970;">
+	V0.50 BETA 5/10/06
+	</div>
+	<br>
+	
+	<br>
+	<br>
+	<div align="center" dir="ltr" class="footer" style="background: cyan; width: 970;">
+	V0.01 BETA 1/8/06
+	</div>
+	<br>
+	הקמת האתר Funworldlife
 	
 		
 		<?php $this->load->view("footer.php", $title) ?>
