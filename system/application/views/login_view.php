8682c25698d35dd65e342e90043360b4304b4397
Nir Lichtman <nir@lichtman.org>
2026-04-08 10:04:55 +0300

    login: translate to english

--- system/application/views/login_view.php
+++ system/application/views/login_view.php
@@ -1,32 +1,25 @@
-﻿
-<?php $this->load->view("header.php", $title) ?>
-	
-		<div dir='rtl' align="center">
-	
+﻿<?php $this->load->view("header.php", $title) ?>
+		<div align="center">
 		<?php echo form_open('login/submit'); ?>
 		<img src='<?php print base_url(); ?>images/Login Main Image.png'>
 		<fieldset>
 		<br>
 		<br>
-		איך מיתחברים? קודם כל עם אין לך שחקן אז אתה צריך להכין שחקן בהרשמה למלא את הפרטים ולזכור את הסיסמה ואת השם מישתמש אחר כך אתה ממלא
-		את הסיסמה שלך ואת השם מישתמש את הסיסמה אסור בהחלט לגלות ואז לילחוץ התחברות עם יש בעיות עם ההתחברות יש לישלוח אימייל לאתר ויענו
-		תוך עד 72 שעות ועם לא אז יש לישלוח שוב הודעה הכתובת מייל: <a href="mailto:admin@funworldlife.com">
-		admin@funworldlife.com
-		</a>
-		תודה.
-
+			How do you login? first of all, if you don't have a user you need to create one. Click on register and fill in the details and remember the password and username.
+			Afterwards, login with your username and password. Do not share your password. Afterwards click "Login". If there login issues, send an email to the website and
+			we'll answer within 72 hours. If not, resend an email. Email address: <a href="mailto:admin@funworldlife.com">admin@funworldlife.com</a>
+			Thanks.
 <br><br><table style="background-image:url(../images/bgdown.png) ; width: 900;" align="center">
-		שם משתמש: <?php echo form_input($username); ?><a href="forgot">
-		שכחתי שם משתמש</a><br>
-		סיסמה: <?php echo form_password($password); ?> <a href="forgot">
-		שכחתי סיסמה</a>
+		Username: <?php echo form_input($username); ?><a href="forgot">
+		Forgot username?</a><br>
+		Password: <?php echo form_password($password); ?> <a href="forgot">
+		Forgot password?</a>
 		<br>
-		<?php echo form_submit('submit', 'התחברות'); ?>
-		<input type='reset' value='אפס טופס'>
+		<?php echo form_submit('submit', 'Login'); ?>
+		<input type='reset' value='Clear form'>
 		</table>
 		<br><br>
 		<br>
-		
 		</fieldset>
 <?php echo form_close(); ?>
 		<a href="register"><img src="<?php print base_url() ?>images/register_login.png"></a>
